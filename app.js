7f34dd0da5a2ba7625862613807a478861394abf
FLOrschiedt <leoorschiedt@gmail.com>
2023-01-08 17:05:12 +0100

    update for posts

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv/config');
 
+//Middlewares
+app.use(cors());
 app.use(bodyParser.json());
 
-//Middlewares
 /*
 app.use('/posts', () => {
     console.log('This is a middleware running')
--- routes/posts.js
+++ routes/posts.js
@@ -2,28 +2,66 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req,res) => {
-    res.send('We are on posts');
+//gibt alle Posts wieder
+router.get('/', async (req,res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch(err) {
+        res.json({message: err});
+    }
 });
 
 router.get('/specific', (req,res) => {
     res.send('Specific post');
 });
 
-router.post('/', (req,res) => {
+//sendet einen Post
+router.post('/', async (req,res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
 
-    post.save()
-    .exec()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
+    try {
+        const savedPost = await post.save()
+        res.json(savedPost);
+    } catch(err) {
         res.json({message: err});
-    });
+    }
+});
+
+//konkreter Post
+router.get('/:postId', async (req,res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch {
+        res.json({message: err});
+    }
+});
+
+//Post löschen
+router.delete('/:postId', async (req,res) => {
+    try {
+        const removedPost = await Post.remove({_id: req.params.postId});
+        res.json(removedPost);
+    } catch {
+        res.json({message: err});
+    }
+});
+
+//Post updaten
+router.patch('/:postId', async (req,res) => {
+    try {
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId}, 
+            {$set: {title: req.body.title}}
+        );
+        res.json(updatedPost);
+    } catch {
+        res.json({message: err});
+    }
 });
 
 module.exports = router;
